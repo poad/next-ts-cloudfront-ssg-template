df4d797cc9ed1f4c11fd886b1ffb3f6c2df3c595
Kenji Saito <ken-yo@mbr.nifty.com>
2023-06-16 08:02:45 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-rm -rf node_modules && pnpm install && pnpm dedupe && pnpm lint-fix && pnpm all
+rm -rf node_modules && pnpm install && pnpm up && pnpm lint-fix && pnpm all
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}"
@@ -35,7 +35,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-rm -rf node_modules && pnpm install && pnpm dedupe && pnpm lint-fix && pnpm build
+rm -rf node_modules && pnpm install && pnpm up && pnpm lint-fix && pnpm build
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}"
