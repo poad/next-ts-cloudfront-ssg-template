61a40a6f142d97f14a91284e598598aceecfca1f
Kenji Saito <ken-yo@mbr.nifty.com>
2024-03-09 12:20:15 +0900

    Bumps node modules

--- cdk/lib/cloudfront-s3-cdn-stack.ts
+++ cdk/lib/cloudfront-s3-cdn-stack.ts
@@ -1,11 +1,11 @@
-import * as crypto from "crypto";
+import * as crypto from "node:crypto";
 import * as cdk from "aws-cdk-lib";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origin from "aws-cdk-lib/aws-cloudfront-origins";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as deployment from "aws-cdk-lib/aws-s3-deployment";
-import { Construct } from "constructs";
+import type { Construct } from "constructs";
 
 export type NextCloudFrontTemplateStackConfig = cdk.StackProps;
 
